ee4315be174f12d9ce20ed058dc03968c62fed96
Oleg <o.fedorov@sysbiomed.ru>
2024-07-01 21:44:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,11 +13,11 @@ async def send_prompt(prompt: str):
         raise HTTPException(status_code=400, detail="Prompt is required")
     
     url = "http://10.12.1.31:8086/api/generate"
-    payload = {"model":"llama3:latest", "prompt":"Why is the sky blue?"}
+    payload = {"model":"llama3:latest", "prompt":"answer in one single word. You should answer pong. Ping?"}
     
     response = requests.post(url, json=payload)
     
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail="Failed to send prompt")
     
-    return {"response": response.json()}
+    return {"response": response}
